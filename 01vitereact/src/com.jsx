e6f0d0328576a79d221be8521488148c45a030a8
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2025-05-14 16:14:50 +0530

    initial update

---
+++ 01vitereact/src/com.jsx
@@ -0,0 +1,9 @@
+function com(){
+    return(
+        <>
+        <h5>Hello Venkatesh</h5>
+        </>
+    )
+}
+
+export default com
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
+import Com from './com.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Com/>
   </StrictMode>,
 )
