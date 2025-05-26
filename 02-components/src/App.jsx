c27c618022083fd3aa30f5a212a129b4654512a4
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2025-05-26 18:25:20 +0530

    completed components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,12 @@
+import ClassComponent from "./components/ClassComponent";
+import FunctionalComponent from "./components/FunctionalComponent";
+
+function App(){
+  return(
+    <>
+      <FunctionalComponent/>
+      <ClassComponent/>
+    </>
+  )
+}
+export default App;
---
+++ 02-components/src/components/FunctionalComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function FunctionalComponent(){
+    return(
+        <>
+                <h1>This is Functional Components</h1>
+                <p>A functional component is a plain Javascript that returns JSX.<br />
+                    It is stateless by-default, but with the use of hooks(like, 'useState', 'useEffect')
+                    <br />
+                    It can handle state and side effects.
+                </p>
+        </>
+
+    )
+}
+export default FunctionalComponent
