06533e1a19f848a154184c31506e6917e62d3497
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2025-08-24 13:26:10 +0530

    Learned About Event Handling

---
+++ 04-event-handling/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React from 'react'
+import {ClickEvent,MouseOverEvent} from './ClickEvent';
+
+const App = () => {
+  return (
+    <div>
+      <ClickEvent/>
+      <MouseOverEvent/>
+    </div>
+  )
+}
+
+export default App
---
+++ 04-event-handling/src/ClickEvent.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+
+export const ClickEvent = () => {
+    const [bgColor, setBgColor] = useState('');
+let r,g,b;
+   const handleColor = ()=>{
+     r = Math.floor(Math.random() * 255 + 1);
+     g = Math.floor(Math.random() * 255 + 1);
+     b = Math.floor(Math.random() * 255 + 1);
+    setBgColor(r + ","+g+","+b);
+   }
+
+   const resetColor = ()=>{
+    r = 0;
+    g = 0;
+    b = 0;
+    setBgColor(r + ","+g+","+b);
+   }
+
+    
+  return (
+    <div
+    style={{
+        width:"200px",
+        height:"200px",
+        backgroundColor:`rgb(${bgColor})`,
+        display:'flex',
+        flexDirection:"column",
+        alignItems:"center",
+        justifyContent:"center",
+        border:"1px solid white"
+    }}
+    >
+      <button onClick={handleColor}>Click me</button>
+      <button onClick={resetColor} style={{
+        marginTop:'10px',
+      }}>Reset</button>
+      {/* <h1>{bgColor}</h1> */}
+    </div>
+  )
+}
+
+
+export const MouseOverEvent = () => {
+    const [bgColor, setBgColor] = useState('');
+
+   const handleColor = ()=>{
+    let r = Math.floor(Math.random() * 255 + 1);
+    let g = Math.floor(Math.random() * 255 + 1);
+    let b = Math.floor(Math.random() * 255 + 1);
+    setBgColor(r + ","+g+","+b);
+   }
+
+   
+
+    
+  return (
+    <div
+    style={{
+        width:"200px",
+        height:"200px",
+        backgroundColor:`rgb(${bgColor})`,
+        display:'flex',
+        flexDirection:"column",
+        alignItems:"center",
+        justifyContent:"center",
+        border:"1px solid white"
+    }}
+    onMouseOver={handleColor}
+    >
+      {/* <button onClick={handleColor}>Click me</button> */}
+      {/* <h1>{bgColor}</h1> */}
+    </div>
+  )
+}
+
+
